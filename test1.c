a9348c01e379817c60214728c39a8406d854c0cb
Robert <robert08bucur@yahoo.com>
2026-02-11 10:35:44 +0000

    Modificarea2

--- test1.c
+++ test1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int suma(int a  , int b){
+    return a + b;
+}
+
 int main(void){
-    printf("Hello world!\n");
+    int a = 4 , b = 7;
+    printf("%d",suma(a,b));
     return 0;
 }
